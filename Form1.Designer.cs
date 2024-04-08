8e514a94d7dc60e6f05d6cac81776c9ddb2178c9
Ortizcarmen <156687528+Ortizcarmen@users.noreply.github.com>
2024-04-08 08:39:01 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Laboratorio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxNota = new System.Windows.Forms.TextBox();
+            this.buttonNota = new System.Windows.Forms.Button();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota:";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(214, 70);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.TabIndex = 2;
+            // 
+            // textBoxNota
+            // 
+            this.textBoxNota.Location = new System.Drawing.Point(214, 129);
+            this.textBoxNota.Name = "textBoxNota";
+            this.textBoxNota.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNota.TabIndex = 3;
+            // 
+            // buttonNota
+            // 
+            this.buttonNota.Location = new System.Drawing.Point(492, 109);
+            this.buttonNota.Name = "buttonNota";
+            this.buttonNota.Size = new System.Drawing.Size(75, 23);
+            this.buttonNota.TabIndex = 4;
+            this.buttonNota.Text = "Agregar Nota";
+            this.buttonNota.UseVisualStyleBackColor = true;
+            this.buttonNota.Click += new System.EventHandler(this.buttonNota_Click);
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(226, 246);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(212, 82);
+            this.buttonGuardar.TabIndex = 5;
+            this.buttonGuardar.Text = "Guardar";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 559);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonNota);
+            this.Controls.Add(this.textBoxNota);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxNota;
+        private System.Windows.Forms.Button buttonNota;
+        private System.Windows.Forms.Button buttonGuardar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio8
+{
+    public partial class Form1 : Form
+    {
+        List<int> notasTemporales = new List<int>();
+        List <NotasAlumno> listaNotas = new List<NotasAlumno>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNota_Click(object sender, EventArgs e)
+        {
+            int nota = Convert.ToInt16(textBoxNota.Text);
+            notasTemporales.Add(nota);
+        }
+
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            //Guarda a un alumno con sus notas
+            NotasAlumno notasAlumno = new NotasAlumno();
+            notasAlumno.Nombre = textBoxNombre.Text;
+            notasAlumno.Notas = notasTemporales.GetRange(0,notasTemporales.Count);
+
+            // Guarda a ese alumno en el listado de notas de alumnos.
+            listaNotas.Add(notasAlumno);
+            Grabar();
+
+            //Borrar las notas temporales para el proximo alumno
+            notasTemporales.Clear();
+        }
+
+        private void Grabar()
+        {
+            string json = JsonConvert.SerializeObject(listaNotas);
+            string archivo = "Datos.json";
+            System.IO.File.WriteAllText(archivo, json);
+        }
+    }
+}
